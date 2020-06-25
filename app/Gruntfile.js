bce9124566373adc9cd76f1a24a1d97189bd5cc2
Tiago Garcia <tiagojsag@gmail.com>
2020-06-25 10:45:19 +0200

    Fix Gruntfile.js code style

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -1,6 +1,8 @@
-module.exports = function (grunt) {
+module.exports = (grunt) => {
 
     grunt.file.setBase('..');
+
+    // eslint-disable-next-line import/no-extraneous-dependencies
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
